ba8a51194a74032f63b4f417c4e2f2d658a725b2
“MK0326” <f20ba008@gmail.com>
2023-10-29 22:25:28 +0900

    sp対応&駒の上書き不可+連続置き対処

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-
 let inputID;
 let outputID;
 let color;
+
 function pawn(value) {
   inputID = value.id;
   color = value.className.split(' ');
 }
 
 function bord(value) {
-  if (inputID == value.id) {
+  
+  if (inputID == value.id && value.className.split(" ")[1] != "checked") {
     value.style.background="light"+color[0];
-    value.className
 
+    // 一コマ目以降置けないように
+    inputID = "";
+    // 駒を置かれていることをクラス名で追加
+    value.classList.add("checked");
   }
 }
